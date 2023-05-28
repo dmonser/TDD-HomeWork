bc13ba1c8ca59d65365357af1a2557cbda43705d
Roman Nosov <romannosov_2001@mail.ru>
2023-05-28 23:12:26 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class PhoneBook {
+
+    private static PhoneBook instance = null;
+
+    private PhoneBook () {
+
+    }
+
+    public static PhoneBook getInstance() {
+        if (instance == null) {
+            instance = new PhoneBook();
+        }
+        return instance;
+    }
+
+    public int add (String name, int number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,33 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class PhoneBookTest {
+    Random random = new Random();
+
+    @ParameterizedTest
+    @MethodSource("addSource")
+    public void testAdd (String name, int number, int expectedStatus) {
+        System.out.println("Start add test");
+        PhoneBook phoneBook = PhoneBook.getInstance();
+
+        int result = phoneBook.add(name, number);
+
+        Assertions.assertEquals(expectedStatus, result);
+    }
+
+    public static Stream<Arguments> addSource() {
+        return Stream.of(
+                Arguments.of("Test Name1", 1986, 1),
+                Arguments.of("Test Name2", 1560, 2),
+                Arguments.of("Test Name3", 2080, 3),
+                Arguments.of("Test Name3", 2014, 3),
+                Arguments.of("Test Name5", 1327, 4)
+        );
+    }
 }
